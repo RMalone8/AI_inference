ab03f16c3c0bcae44ba6b868ed4d3402336fe586
ryan <ryan@example.com>
2025-05-30 15:05:40 -0400

    Anaylzing parking signs

--- metrics.py
+++ metrics.py
@@ -7,10 +7,12 @@ podman_client = podman.PodmanClient(base_url="unix:///tmp/podman.sock")
 
 containers = podman_client.containers.list()
 
-CPU_USAGE1 = Gauge('cpu_usage_gemma', 'CPU ....')
-MEM_USAGE1 = Gauge('mem_usage_gemma', 'Mem ....')
+CPU_USAGE1 = Gauge('cpu_usage_llava_phi', 'CPU ....')
+MEM_USAGE1 = Gauge('mem_usage_llava_phi', 'Mem ....')
+AVG_CPU_USAGE1 = Gauge('avg_cpu_usage_llava_phi', 'CPU ....')
 CPU_USAGE2 = Gauge('cpu_usage_moondream', 'CPU ....')
 MEM_USAGE2 = Gauge('mem_usage_moondream', 'Mem ....')
+AVG_CPU_USAGE2 = Gauge('avg_cpu_usage_moondream', 'CPU ....')
 
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
@@ -18,14 +20,17 @@ if __name__ == '__main__':
     # Generate some requests.
 
     for container in containers:
-        if container.name == "composetest-ollama-server-gemma-1":
+        if container.name == "composetest-ollama-server-llava-phi-1":
             stats1 = container.stats(stream=True, decode=True)
         if container.name == "composetest-ollama-server-moondream-1":
             stats2 = container.stats(stream=True, decode=True)
 
     for stat1, stat2 in zip(stats1, stats2):
         CPU_USAGE1.set(stat1["Stats"][0].get("CPU"))
-        MEM_USAGE1.set(stat1["Stats"][0].get("MemPerc"))
+        MEM_USAGE1.set(stat1["Stats"][0].get("MemUsage"))
+        AVG_CPU_USAGE1.set(stat1["Stats"][0].get("AvgCPU"))
         CPU_USAGE2.set(stat2["Stats"][0].get("CPU"))
-        MEM_USAGE2.set(stat2["Stats"][0].get("MemPerc"))
-        time.sleep(1)
+        MEM_USAGE2.set(stat2["Stats"][0].get("MemUsage"))
+        AVG_CPU_USAGE2.set(stat2["Stats"][0].get("AvgCPU"))
+        #print(stat1)
+        time.sleep(0.5)
