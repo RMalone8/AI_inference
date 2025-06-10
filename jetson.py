1e993777e365abd8b5c4c8c8f80153005e9ec828
ryan <ryan@example.com>
2025-06-10 15:35:58 -0400

    optimize script

--- jetson.py
+++ jetson.py
@@ -4,42 +4,47 @@ from jumpstarter_driver_network.adapters import FabricAdapter
 
 from jumpstarter.utils.env import env
 
+from fabric import Config
+
 HOSTNAME = "localhost"
 USERNAME = "admin"
 PASSWORD = "passwd"
+POWERCYCLE = False
 
 # init jumpstarter client from env (jmp shell)
 with env() as dut:
-    # ensure the usb dirve is connected to the dut
-    dut.storage.dut()
+    if POWERCYCLE:
+        # ensure the usb dirve is connected to the dut
+        dut.storage.dut()
 
-    # open serial console
-    with dut.serial.pexpect() as p:
-        # forward console output to stdout
-        p.logfile = sys.stdout.buffer
+        # open serial console
+        with dut.serial.pexpect() as p:
+            # forward console output to stdout
+            p.logfile = sys.stdout.buffer
 
-        # power cycle dut
-        dut.power.cycle()
+            # power cycle dut
+            dut.power.cycle()
 
-        # wait for firmware
-        p.expect_exact("Enter to continue boot.", timeout=120)
-        p.sendline("")
+            # wait for firmware
+            p.expect_exact("Enter to continue boot.", timeout=120)
+            p.sendline("")
 
-        # wait for login prompt
-        p.expect_exact("login: ", timeout=120)
-        p.sendline(USERNAME)
-        p.expect_exact("Password: ", timeout=120)
-        p.sendline(PASSWORD)
-        p.expect_exact(f"[{USERNAME}@{HOSTNAME} ~]$", timeout=120)
+            # wait for login prompt
+            p.expect_exact("login: ", timeout=120)
+            p.sendline(USERNAME)
+            p.expect_exact("Password: ", timeout=120)
+            p.sendline(PASSWORD)
+            p.expect_exact(f"[{USERNAME}@{HOSTNAME} ~]$", timeout=120)
 
     # connect to ssh
     with FabricAdapter(
         client=dut.ssh,
         user=USERNAME,
+        config=Config(overrides={'sudo': {'password': PASSWORD}}),
         connect_kwargs={
             "password": PASSWORD,
         },
     ) as ssh:
         # run command over ssh
-        result = ssh.run("uname -a")
+        result = ssh.sudo("podman images")
         print(result)
