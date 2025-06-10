7f70fc1d68ec1346443ea9f9d69bd928ab041628
ryan <ryan@example.com>
2025-06-10 15:27:01 -0400

    add jetson.py script

---
+++ jetson.py
@@ -0,0 +1,45 @@
+import sys
+
+from jumpstarter_driver_network.adapters import FabricAdapter
+
+from jumpstarter.utils.env import env
+
+HOSTNAME = "localhost"
+USERNAME = "admin"
+PASSWORD = "passwd"
+
+# init jumpstarter client from env (jmp shell)
+with env() as dut:
+    # ensure the usb dirve is connected to the dut
+    dut.storage.dut()
+
+    # open serial console
+    with dut.serial.pexpect() as p:
+        # forward console output to stdout
+        p.logfile = sys.stdout.buffer
+
+        # power cycle dut
+        dut.power.cycle()
+
+        # wait for firmware
+        p.expect_exact("Enter to continue boot.", timeout=120)
+        p.sendline("")
+
+        # wait for login prompt
+        p.expect_exact("login: ", timeout=120)
+        p.sendline(USERNAME)
+        p.expect_exact("Password: ", timeout=120)
+        p.sendline(PASSWORD)
+        p.expect_exact(f"[{USERNAME}@{HOSTNAME} ~]$", timeout=120)
+
+    # connect to ssh
+    with FabricAdapter(
+        client=dut.ssh,
+        user=USERNAME,
+        connect_kwargs={
+            "password": PASSWORD,
+        },
+    ) as ssh:
+        # run command over ssh
+        result = ssh.run("uname -a")
+        print(result)
