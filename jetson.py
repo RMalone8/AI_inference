73685e24f4c6aca057b039837ca7d103f2c272c5
ryan <ryan@example.com>
2025-06-11 16:15:30 -0400

    install jmp

---
+++ jetson.py
@@ -0,0 +1,58 @@
+import sys
+import os
+
+from jumpstarter_driver_network.adapters import FabricAdapter, TcpPortforwardAdapter
+
+from jumpstarter.utils.env import env
+
+from invoke import run
+import podman
+
+HOSTNAME = "localhost"
+USERNAME = "admin"
+PASSWORD = "passwd"
+POWERCYCLE = False
+
+# init jumpstarter client from env (jmp shell)
+with env() as dut:
+    if POWERCYCLE:
+        # ensure the usb dirve is connected to the dut
+        dut.storage.dut()
+
+        # open serial console
+        with dut.serial.pexpect() as p:
+            # forward console output to stdout
+            p.logfile = sys.stdout.buffer
+
+            # power cycle dut
+            dut.power.cycle()
+
+            # wait for firmware
+            p.expect_exact("Enter to continue boot.", timeout=120)
+            p.sendline("")
+
+            # wait for login prompt
+            p.expect_exact("login: ", timeout=120)
+            p.sendline(USERNAME)
+            p.expect_exact("Password: ", timeout=120)
+            p.sendline(PASSWORD)
+            p.expect_exact(f"[{USERNAME}@{HOSTNAME} ~]$", timeout=120)
+
+    # connect to ssh
+    with FabricAdapter(
+        client=dut.ssh,
+        user=USERNAME,
+        connect_kwargs={
+            "password": PASSWORD,
+        },
+    ) as ssh:
+        # run command over ssh
+        # ssh.sudo("systemctl enable --now podman.socket")
+        # ssh.sudo("chmod -R 0777 /run/podman")
+        # ssh.shell()
+        ssh.forward_local(9090, 9091, local_host="0.0.0.0")
+        with TcpPortforwardAdapter(client=dut.ssh) as addr:
+            os.environ["CONTAINER_HOST"] = f"ssh://{USERNAME}@{addr[0]}:{addr[1]}/run/podman/podman.sock"
+            os.environ["CONTAINER_SSHKEY"] = "/Users/rmalone/.ssh/id_ed25519"
+            run("podman images")
+            run("./startup.sh")
